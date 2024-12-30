77ff244fdb5b6250c3c8beef6964819dd8957ee4
sharath160k@gmail.com <sharath160k@gmail.com>
2024-12-30 11:21:32 +0530

    pagination initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [listOfTodos, setListOfTodos] = useState([]);
+  const [numOfTodosPerPage, setNumOfTodosPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => setListOfTodos(res.data));
+  }, []);
+  const numOfTotalPages = Math.ceil(listOfTodos.length / numOfTodosPerPage);
+  const pages1 = [...Array(numOfTotalPages + 1).keys()].slice(1);
+  const lastIndex = currentPage * numOfTodosPerPage;
+  const firstIndex = lastIndex - numOfTodosPerPage;
+  const visibleTodos = listOfTodos.slice(firstIndex, lastIndex);
+
+  const handlePrevPage = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const handleNextPage = () => {
+    if (currentPage !== 20) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={(e) => setNumOfTodosPerPage(e.target.value)}>
+        <option value="" key="">
+          10
+        </option>
+        <option>20</option>
+        <option>30</option>
+        <option>40</option>
+        <option>50</option>
+      </select>
+      {visibleTodos.map((todo) => (
+        <p key={todo.id}>{todo.title}</p>
+      ))}
+      <p onClick={handlePrevPage}>prev</p>
+      {pages1.map((page) => (
+        <span
+          key={page}
+          className={`${currentPage === page ? "active" : ""}`}
+          onClick={() => setCurrentPage(page)}
+        >{`${page} |`}</span>
+      ))}
+      <p onClick={handleNextPage}>next</p>
     </div>
   );
 }
